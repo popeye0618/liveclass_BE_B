52d59a412d9bca5911c60f500a399309a2259ed5
popeye0618 <popeye0618@gmail.com>
2026-05-21 11:48:46 +0900

    feat(build): 초기 Spring Boot 프로젝트 설정 추가
    Spring Boot 기반의 프로젝트를 초기화하고 필요한 설정과 의존성을 추가했습니다.

---
+++ src/main/java/hello/liveclass_be_b/LiveclassBeBApplication.java
@@ -0,0 +1,13 @@
+package hello.liveclass_be_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveclassBeBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveclassBeBApplication.class, args);
+    }
+
+}
